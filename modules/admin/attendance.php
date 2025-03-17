bf7b16d74a6bcea7fe62fd94ad38743655479c5f
Skorpion <kirankumar82054@gmail.com>
2025-03-18 00:21:14 +0530

    Update attendance.php

--- modules/admin/attendance.php
+++ modules/admin/attendance.php
@@ -40,7 +40,7 @@ foreach ($attendance_records as &$record) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Attendance - Admin</title>
-    <link rel="stylesheet" href="modules/admin/styles/attendance.css">
+    <link rel="stylesheet" href="modules/admin/styles/admin.css">
 </head>
 <body>
 
